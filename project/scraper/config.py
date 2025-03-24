e4e6145dd0c83e8379edc8a329abf455ad7d6c02
Carsten Stahl <carstenstahl307@yahoo.com>
2025-03-24 15:49:18 +0100

    added config for Scrapers that contains access to Zeeschuimer extension

---
+++ project/scraper/config.py
@@ -0,0 +1,2 @@
+ZEESCHUIMER_URL = "https://github.com/digitalmethodsinitiative/zeeschuimer/releases/download/v1.12.2/zeeschuimer-v1.12.2.xpi"
+ZEESCHUIMER_ID = "zeeschuimer@9a5ba22d-c08f-494f-86c2-e9fd04a6a42c"
